d80a41db9cc73b48836c47fd197be3a341031f60
hung222 <90371118+hung222@users.noreply.github.com>
2023-02-05 20:03:24 +0700

    Update settings.py

--- alphaiu/settings.py
+++ alphaiu/settings.py
@@ -130,3 +130,4 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
